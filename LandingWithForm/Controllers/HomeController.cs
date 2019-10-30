08012bde85d67439f7446a300abc5749e75fd945
Владислав Горохов <gbg.0101@mail.ru>
2019-10-30 18:22:30 +0600

    first commit

---
+++ LandingWithForm/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LandingWithForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SendToEmail(string name, string email, string subject, string body)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.mail.ru");
+
+                mail.From = new MailAddress("gvo_step2018@mail.ru");
+                mail.To.Add(email);
+                mail.Subject = $"User: {name}, Subject: {subject}";
+                mail.Body = body;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("gvo_step2018@mail.ru", "8W8_55Vlad");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return View("index");
+            }
+            catch (Exception exception)
+            {
+                ViewBag.Error = exception.Message;
+                return View("Error");
+            }
+        }
+    }
+}
